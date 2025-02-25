8e6a6c7a10a7066ed18eca630dce69040a9d8dd9
Scherbakov.M <@v1n1sk0>
2025-02-25 14:40:07 +0300

    dead inside true ghoul kaneki SSS rank 1000-7 (try №21)

--- src/processing.py
+++ src/processing.py
@@ -8,11 +8,9 @@ def filter_by_state(list_of_dicts: list[Any], state: str = "EXECUTED") -> list[A
             correct_list.append(list_of_dicts[num_dict])
     return correct_list
 
-
 def sort_by_date(list_of_dicts: list[Any], sorting_direct: bool = True) -> list[Any]:
     """Сортирует список по датам в словарях по убыванию (по умолчанию)"""
     return sorted(list_of_dicts, key=lambda x: x.get("date"), reverse=sorting_direct)
-
 print(
     filter_by_state(
         [
